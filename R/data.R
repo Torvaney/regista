bec4c745c85794952e8075e47ab34f7fc5b53ae8
Torvaney <torvaney@protonmail.com>
2018-05-07 17:33:15 +0100

    Add example data
    Add PL 2010/11 data for testing and for examples.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Results for the 2010/11 English Premier League season
+#'
+#' A dataset containing the home and away goals for each game in the 2010/11
+#' English Premier League season.
+#'
+#' @format A data frame with 380 rows and 6 variables:
+#' \describe{
+#'   \item{date}{date that the game was played}
+#'   \item{home}{The home team}
+#'   \item{away}{The away team}
+#'   \item{hgoal}{The number of goals scored by the home team}
+#'   \item{agoal}{The number of goals scored by the away team}
+#'   \item{result}{The match result (H/D/A)}
+#' }
+"premier_league_2010"
