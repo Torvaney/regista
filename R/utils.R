2f3f7ed17de1eaaf8ceca57438475692e2a37766
Torvaney <torvaney@protonmail.com>
2018-08-16 00:15:46 +0100

    Use tidyeval in scoreline calc

--- R/utils.R
+++ R/utils.R
@@ -50,14 +50,17 @@ def <- function(x) {
 #' @param agoal The column containing away goals
 #' @param prob The column containing the probability of that given scoreline
 #'
-#' @importFrom lazyeval f_eval
+#' @importFrom rlang eval_tidy quo
 #' @importFrom purrr map_dbl
 #' @importFrom glue glue
 #'
 #' @export
-scorelines_to_outcomes <- function(scorelines, hgoal = ~hgoal, agoal = ~agoal, prob = ~prob) {
-  hgoals <- f_eval(hgoal, scorelines)
-  agoals <- f_eval(agoal, scorelines)
+scorelines_to_outcomes <- function(scorelines,
+                                   hgoal = quo(hgoal),
+                                   agoal = quo(agoal),
+                                   prob  = quo(prob)) {
+  hgoals <- eval_tidy(hgoal, scorelines)
+  agoals <- eval_tidy(agoal, scorelines)
 
   # Determine the result for each scoreline
   scorelines$outcome <- NA_character_
@@ -66,17 +69,16 @@ scorelines_to_outcomes <- function(scorelines, hgoal = ~hgoal, agoal = ~agoal, p
   scorelines$outcome <- ifelse(agoals == hgoals, "draw", scorelines$outcome)
 
   sum_probs <- function(outcome) {
-    sum(f_eval(prob, scorelines[scorelines$outcome == outcome, ]))
+    sum(eval_tidy(prob, scorelines[scorelines$outcome == outcome, ]))
   }
 
   # NOTE: Should outcomes be a factor?
   #       There are 3 clear levels and not all of them are guaranteed to appear
   #       Tidyverse convention appears to be to favour characters
   outcomes <- c("home_win", "draw", "away_win")
-  outcome_probs <- data.frame(
+  outcome_probs <- tibble::tibble(
     outcome = outcomes,
-    prob    = map_dbl(outcomes, sum_probs),
-    stringsAsFactors = FALSE
+    prob    = map_dbl(outcomes, sum_probs)
   )
 
   if (sum(outcome_probs$prob) != 1) {
